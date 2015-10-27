3a16dad4e6e9be12c647101d4d9d15deea7a6119
Whitesunset <chester.work@gmail.com>
2015-10-27 15:48:33 +0100

    Test: One more try to update submodule

---
+++ Response.php
@@ -0,0 +1,23 @@
+<?php
+namespace Awesome;
+
+class Response {
+    public static function get(\WP_Ajax_Response $response, $data, $type){
+        if($type == 'errors'){
+            $response->add(array(
+                    'what' => $type,
+                    'id' => $data
+                ));
+        }else{
+            $response->add(array(
+                    'what'=> $type,
+                    'action'=> $type,
+                    'id'=>'1',
+                    'data'=> $data,
+                    'supplemental' => ''
+                ));
+        }
+        return $response;
+    }
+
+} 
